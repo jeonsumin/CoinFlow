92328561d71c17ab783d0f012b22a1e8df69dd1c
jeonsumin <soomis7@naver.com>
2020-11-30 23:38:36 +0900

    chartDetail view Design

--- CoinFlow-Terry/Chart/ChartDetailViewController.swift
+++ CoinFlow-Terry/Chart/ChartDetailViewController.swift
@@ -8,13 +8,47 @@
 import UIKit
 
 class ChartDetailViewController: UIViewController {
-
+    
     var coinInfo : CoinInfo!
+    
+    @IBOutlet weak var coinTypeLabel: UILabel!
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    @IBOutlet weak var highlightBar: UIView!
+    @IBOutlet weak var heighlightBarLeading: NSLayoutConstraint!
     
     //MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateCoinInfo(coinInfo)
+        fetchData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        print("넘어온 데이터 ::: \(coinInfo.key)")
+    }
+    
+    @IBAction func dailyButtonTapped(_ sender: UIButton) {
+        moveHightlightBar(to: sender)
+    }
+    @IBAction func weeklyButtonTapped(_ sender: UIButton) {
+        moveHightlightBar(to: sender)
+    }
+    @IBAction func monthlyButtonTapped(_ sender: UIButton) {
+        moveHightlightBar(to: sender)
+    }
+    @IBAction func yearlyButtonTapped(_ sender: UIButton) {
+        moveHightlightBar(to: sender)
+    }
+    
+    
+}
 
+//MARK: private Method
+extension ChartDetailViewController {
+    
+    private func fetchData(){
         NetworkManager.requestCoinChartData { result in
             switch result {
             case .success(let coinChart):
@@ -25,8 +59,12 @@ class ChartDetailViewController: UIViewController {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("넘어온 데이터 ::: \(coinInfo.key)")
+    private func updateCoinInfo(_ coinInfo: CoinInfo) {
+        coinTypeLabel.text = "\(coinInfo.key)"
+        currentPriceLabel.text = String(format: "%.1f", coinInfo.value.usd.price)
+    }
+    
+    private func moveHightlightBar(to button: UIButton){
+        heighlightBarLeading.constant = button.frame.minX
     }
 }
